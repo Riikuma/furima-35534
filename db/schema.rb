a53cd4ca614a8bca2cac31ac9061e66d98a3a84c
Riikuma <miracle080510@gmail.com>
2021-05-17 17:04:53 +0900

    テーブルの生年月日の型をintegerからdateへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_023347) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.integer "user_birth_date", null: false
+    t.date "user_birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
