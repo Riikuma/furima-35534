23cc6a9d8fa73b0f3e199fd51ca21057da17c9fe
Riikuma <miracle080510@gmail.com>
2021-05-18 01:45:25 +0900

    ユーザー情報をFactoryBotで指定した一部の記述を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
     Faker::Config.locale = :ja
+    transient do
+      person { Gimei.name }
+    end
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
@@ -8,8 +11,8 @@ FactoryBot.define do
     nickname {Faker::Name.name}
     last_name {Faker::Name.last_name}
     first_name {Faker::Name.first_name}
-    last_name_kana {Faker::Name.last_kana_name}
-    first_name_kana {Faker::Name.first_kana_name}
-    user_birth_date {Faker::Date.between_except(from: '1930-01-01', to: "#{Time.now.year - 5}-12-31")}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    user_birth_date {Faker::Date.between(from: '1930-01-01', to: "#{Time.now.year - 5}-12-31")}
   end
 end
