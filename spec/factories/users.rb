72083da369ff7fc76db4f1fa7000d64cf084f4c0
Riikuma <miracle080510@gmail.com>
2021-05-18 12:43:30 +0900

    テストコード追加、修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,15 +4,14 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    nickname {Faker::Name.name}
-    last_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
-    last_name_kana {person.last.katakana}
-    first_name_kana {person.first.katakana}
-    user_birth_date {Faker::Date.between(from: '1930-01-01', to: "#{Time.now.year - 5}-12-31")}
+    email { Faker::Internet.free_email }
+    password { '1a' + Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
+    nickname { Faker::Name.name }
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
+    user_birth_date { Faker::Date.between(from: '1930-01-01', to: "#{Time.now.year - 5}-12-31") }
   end
 end
