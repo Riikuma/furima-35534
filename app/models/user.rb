2f0d218a4a781e76d24d6017a5453b1a89f3371b
Riikuma <miracle080510@gmail.com>
2021-05-18 12:48:43 +0900

    バリデーション表記修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
-  validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください', allow_blank: true } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+  with_options presence: true do
+    validates :nickname
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください', allow_blank: true } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カナ文字を使用してください', allow_blank: true } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :user_birth_date
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'には全角カナ文字を使用してください', allow_blank: true } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-  validates :user_birth_date, presence: true
 end
