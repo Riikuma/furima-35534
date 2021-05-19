799a64c784c24cb1ddc7f070687aa31da37ca223
Riikuma <miracle080510@gmail.com>
2021-05-19 18:32:47 +0900

    ビューファイル修正、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,8 @@ class Item < ApplicationRecord
     with_options numericality: { other_than: 1, message: 'には[---]以外を選択してください', allow_blank: true } do
       validates :item_category_id, :item_status_id, :item_shipping_fee_status_id, :prefecture_id, :item_days_to_ship_id
     end
-    validates :item_price, format: { with: /\A[0-9]+\z/, message: 'には半角英数字を入力してください', allow_blank: true },
-                           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'には300~9999999の範囲で入力してください', allow_blank: true }
+    validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                                           message: 'には半角数字を300~9999999の範囲で入力してください', allow_blank: true }
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
