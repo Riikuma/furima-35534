04a0b836d9843ea4d0db219b7d6ddc02f4b68b14
Riikuma <miracle080510@gmail.com>
2021-05-20 19:57:45 +0900

    Rubocop自動修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     item_shipping_fee_status_id { Faker::Number.within(range: 2..3) }
     prefecture_id { Faker::Number.within(range: 2..48) }
     item_days_to_ship_id { Faker::Number.within(range: 2..4) }
-    item_price { Faker::Number.within(range: 300..9999999) }
+    item_price { Faker::Number.within(range: 300..9_999_999) }
     association :user
 
     after(:build) do |item|
