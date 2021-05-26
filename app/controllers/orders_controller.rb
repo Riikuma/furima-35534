15814951789f519caae2131767fb8a864d29c124
Riikuma <miracle080510@gmail.com>
2021-05-26 23:21:13 +0900

    ordersコントローラー追加、処理を記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:buyer_postal_code, :prefecture, :buyer_city, :buyer_address, :buyer_building, :buyer_phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
