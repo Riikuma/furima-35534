c369a5e67d5f73e461b469bad6b875fe03623a30
Riikuma <miracle080510@gmail.com>
2021-05-27 17:03:17 +0900

    フォームオブジェクトのバリデーションエラー文編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order
     validates :buyer_postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'にはハイフン(-)を含めて半角数字で入力してください', allow_blank: true }
     validates :prefecture_id, numericality: { other_than: 1, message: 'には[---]以外を選択してください', allow_blank: true }
     validates :buyer_city, :buyer_address
-    validates :buyer_phone_number, format: { with: /\A\d{10,11}\z/, message: 'には10桁もしくは11桁で入力してください', allow_blank: true }
+    validates :buyer_phone_number, format: { with: /\A\d{10,11}\z/, message: 'には半角数字のみを用いて10桁もしくは11桁を入力してください', allow_blank: true }
     validates :user_id, :item_id
   end
 
