4ea411bc3932ece9a83ab2765ad0395c08262ad1
Riikuma <miracle080510@gmail.com>
2021-05-27 14:59:27 +0900

    オーダークラス（モデル）のFactoryBotを追加

---
+++ spec/factories/orders.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order do
+    Faker::Config.locale = :ja
+    token { 'tok_abcdefghijk00000000000000000' }
+    buyer_postal_code { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    buyer_city { Faker::Address.city }
+    buyer_address { Faker::Address.street_address }
+    buyer_building { Faker::Address.secondary_address }
+    buyer_phone_number { Faker::Number.number(digits: 11) }
+    user_id { Faker::Number.within(range: 1..100) }
+    item_id { Faker::Number.within(range: 1..100) }
+  end
+end
