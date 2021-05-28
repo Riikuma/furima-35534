3135daaffad3c53dea2f2686624d09fecca47c1d
Riikuma <miracle080510@gmail.com>
2021-05-28 18:15:36 +0900

    テストコードを日本語化に対応

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,115 +16,115 @@ RSpec.describe Item, type: :model do
       it '商品画像が空では出品できないこと' do
         @item.item_image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item image can't be blank")
+        expect(@item.errors.full_messages).to include('商品画像を入力してください')
       end
 
       it '商品名が空では出品できないこと' do
         @item.item_name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item name can't be blank")
+        expect(@item.errors.full_messages).to include('商品名を入力してください')
       end
       it '商品名が40文字を超えると出品できないこと' do
         @item.item_name = Faker::Lorem.paragraph_by_chars(number: 41, supplemental: false)
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item name には40文字以内で入力してください')
+        expect(@item.errors.full_messages).to include('商品名は40文字以内で入力してください')
       end
 
       it '商品の説明が空では出品できないこと' do
         @item.item_description = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item description can't be blank")
+        expect(@item.errors.full_messages).to include('商品の説明を入力してください')
       end
       it '商品の説明が1000文字を超えると出品できないこと' do
         @item.item_description = Faker::Lorem.paragraph_by_chars(number: 1001, supplemental: false)
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item description には1000文字以内で入力してください')
+        expect(@item.errors.full_messages).to include('商品の説明は1000文字以内で入力してください')
       end
 
       it '商品のカテゴリー情報が空では出品できないこと' do
         @item.item_category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item category can't be blank")
+        expect(@item.errors.full_messages).to include('商品のカテゴリーを入力してください')
       end
       it '商品のカテゴリー情報が初期状態（idが1、nameが---）では出品できないこと' do
         @item.item_category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item category には[---]以外を選択してください')
+        expect(@item.errors.full_messages).to include('商品のカテゴリーは[---]以外を選択してください')
       end
 
       it '商品の状態についての情報が空では出品できないこと' do
         @item.item_status_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item status can't be blank")
+        expect(@item.errors.full_messages).to include('商品の状態を入力してください')
       end
       it '商品の状態についての情報が初期状態（idが1、nameが---）では出品できないこと' do
         @item.item_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item status には[---]以外を選択してください')
+        expect(@item.errors.full_messages).to include('商品の状態は[---]以外を選択してください')
       end
 
       it '商品の配送料の負担についての情報が空では出品できないこと' do
         @item.item_shipping_fee_status_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
+        expect(@item.errors.full_messages).to include('配送料の負担を入力してください')
       end
       it '商品の配送料の負担についての情報が初期状態（idが1、nameが---）では出品できないこと' do
         @item.item_shipping_fee_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item shipping fee status には[---]以外を選択してください')
+        expect(@item.errors.full_messages).to include('配送料の負担は[---]以外を選択してください')
       end
 
       it '商品の発送元の地域についての情報が空では出品できないこと' do
         @item.prefecture_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item.errors.full_messages).to include('発送元の地域を入力してください')
       end
       it '商品の発送元の地域についての情報が初期状態（idが1、nameが---）では出品できないこと' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture には[---]以外を選択してください')
+        expect(@item.errors.full_messages).to include('発送元の地域は[---]以外を選択してください')
       end
 
       it '商品の発送までの日数についての情報が空では出品できないこと' do
         @item.item_days_to_ship_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item days to ship can't be blank")
+        expect(@item.errors.full_messages).to include('発送までの日数を入力してください')
       end
       it '商品の発送までの日数についての情報が初期状態（idが1、nameが---）では出品できないこと' do
         @item.item_days_to_ship_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item days to ship には[---]以外を選択してください')
+        expect(@item.errors.full_messages).to include('発送までの日数は[---]以外を選択してください')
       end
 
       it '商品の販売価格についての情報が空では出品できないこと' do
         @item.item_price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item price can't be blank")
+        expect(@item.errors.full_messages).to include('販売価格を入力してください')
       end
       it '商品の販売価格が¥300未満では出品できないこと' do
         @item.item_price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price には半角数字を300~9999999の範囲で入力してください')
+        expect(@item.errors.full_messages).to include('販売価格は半角数字のみを用いて300~9999999の範囲で入力してください')
       end
       it '商品の販売価格が¥9,999,999より大きいと出品できないこと' do
         @item.item_price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price には半角数字を300~9999999の範囲で入力してください')
+        expect(@item.errors.full_messages).to include('販売価格は半角数字のみを用いて300~9999999の範囲で入力してください')
       end
       it '商品の販売価格が全角数字では出品できないこと' do
         @item.item_price = '８７２４９６１'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price には半角数字を300~9999999の範囲で入力してください')
+        expect(@item.errors.full_messages).to include('販売価格は半角数字のみを用いて300~9999999の範囲で入力してください')
       end
       it '商品の販売価格が漢数字では出品できないこと' do
         @item.item_price = '二十九万八千六百'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price には半角数字を300~9999999の範囲で入力してください')
+        expect(@item.errors.full_messages).to include('販売価格は半角数字のみを用いて300~9999999の範囲で入力してください')
       end
       it '商品の販売価格が英字では出品できないこと' do
         @item.item_price = 'thirteen hundred and twenty'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price には半角数字を300~9999999の範囲で入力してください')
+        expect(@item.errors.full_messages).to include('販売価格は半角数字のみを用いて300~9999999の範囲で入力してください')
       end
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -32,84 +32,84 @@ RSpec.describe Order, type: :model do
       it 'トークンが空（クレジットカード情報の入力が不完全）では購入できないこと' do
         @order.token = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Token can't be blank")
+        expect(@order.errors.full_messages).to include('クレジットカード情報を入力してください')
       end
 
       it '郵便番号が空では購入できないこと' do
         @order.buyer_postal_code = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Buyer postal code can't be blank")
+        expect(@order.errors.full_messages).to include('郵便番号を入力してください')
       end
       it '郵便番号にハイフンがないと購入できないこと' do
         @order.buyer_postal_code = '1234567'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer postal code にはハイフン(-)を含めて半角数字で入力してください')
+        expect(@order.errors.full_messages).to include('郵便番号はハイフン(-)を含めて半角数字で入力してください')
       end
       it '郵便番号が全角数字では購入できないこと' do
         @order.buyer_postal_code = '１２３-４５６７'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer postal code にはハイフン(-)を含めて半角数字で入力してください')
+        expect(@order.errors.full_messages).to include('郵便番号はハイフン(-)を含めて半角数字で入力してください')
       end
 
       it '都道府県が空では購入できないこと' do
         @order.prefecture_id = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@order.errors.full_messages).to include('都道府県を入力してください')
       end
       it '都道府県が初期状態（idが1、nameが---）では購入できないこと' do
         @order.prefecture_id = 1
         @order.valid?
-        expect(@order.errors.full_messages).to include('Prefecture には[---]以外を選択してください')
+        expect(@order.errors.full_messages).to include('都道府県は[---]以外を選択してください')
       end
 
       it '市区町村が空では購入できないこと' do
         @order.buyer_city = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Buyer city can't be blank")
+        expect(@order.errors.full_messages).to include('市区町村を入力してください')
       end
 
       it '番地が空では購入できないこと' do
         @order.buyer_address = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Buyer address can't be blank")
+        expect(@order.errors.full_messages).to include('番地を入力してください')
       end
 
       it '電話番号が空では購入できないこと' do
         @order.buyer_phone_number = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Buyer phone number can't be blank")
+        expect(@order.errors.full_messages).to include('電話番号を入力してください')
       end
       it '電話番号が12桁以上では購入できないこと' do
         @order.buyer_phone_number = '090123456789'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer phone number には半角数字のみを用いて10桁もしくは11桁を入力してください')
+        expect(@order.errors.full_messages).to include('電話番号は半角数字のみを用いて10桁もしくは11桁で入力してください')
       end
       it '電話番号が9桁以下では購入できないこと' do
         @order.buyer_phone_number = '090123456'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer phone number には半角数字のみを用いて10桁もしくは11桁を入力してください')
+        expect(@order.errors.full_messages).to include('電話番号は半角数字のみを用いて10桁もしくは11桁で入力してください')
       end
       it '電話番号が半角英数字混合では購入できないこと' do
         @order.buyer_phone_number = 'o9012345678'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer phone number には半角数字のみを用いて10桁もしくは11桁を入力してください')
+        expect(@order.errors.full_messages).to include('電話番号は半角数字のみを用いて10桁もしくは11桁で入力してください')
       end
       it '電話番号が全角数字では購入できないこと' do
         @order.buyer_phone_number = '０９０１２３４５６７８'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Buyer phone number には半角数字のみを用いて10桁もしくは11桁を入力してください')
+        expect(@order.errors.full_messages).to include('電話番号は半角数字のみを用いて10桁もしくは11桁で入力してください')
       end
 
       it 'user_idが空では購入できないこと' do
         @order.user_id = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("User can't be blank")
+        expect(@order.errors.full_messages).to include('ユーザー情報を入力してください')
       end
 
       it 'item_idが空では購入できないこと' do
         @order.item_id = ''
         @order.valid?
-        expect(@order.errors.full_messages).to include("Item can't be blank")
+        expect(@order.errors.full_messages).to include('商品情報を入力してください')
       end
     end
   end
