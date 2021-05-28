876bc034273ff3f4e7de414c1dbff18c6d2d86d6
Riikuma <miracle080510@gmail.com>
2021-05-28 17:40:57 +0900

    バリデーションの順序変更、リファクタリング、エラー文編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
   with_options presence: true do
     validates :item_image
-    validates :item_name, length: { maximum: 40, message: 'には40文字以内で入力してください', allow_blank: true }
-    validates :item_description, length: { maximum: 1000, message: 'には1000文字以内で入力してください', allow_blank: true }
-    with_options numericality: { other_than: 1, message: 'には[---]以外を選択してください', allow_blank: true } do
+    validates :item_name, length: { maximum: 40, message: 'は40文字以内で入力してください', allow_blank: true }
+    validates :item_description, length: { maximum: 1000, message: 'は1000文字以内で入力してください', allow_blank: true }
+    with_options numericality: { other_than: 1, message: 'は[---]以外を選択してください', allow_blank: true } do
       validates :item_category_id, :item_status_id, :item_shipping_fee_status_id, :prefecture_id, :item_days_to_ship_id
     end
     validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                                           message: 'には半角数字を300~9999999の範囲で入力してください', allow_blank: true }
+                                           message: 'は半角数字のみを用いて300~9999999の範囲で入力してください', allow_blank: true }
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
